2bbd8c4bda3eea1275d733f37b81c6704e60c5e1
Sarode Hemali S <85745635+Sarodehs@users.noreply.github.com>
2023-11-04 18:13:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './component/User/Home';
 import Contactus from './component/User/Contactus';
@@ -37,7 +36,6 @@ import PressRelease from './component/User/PressRelease';
 
 // admin
 import AdminDashboard from "./component/Admin/AdminDashboard";
-import AdminTicketsView from "./component/Admin/AdminTicketsView";
 import AdminUser from './component/Admin/AdminUser';
 import AdminHeadline from './component/Admin/AdminHeadline';
 import AdminSpsMessage from './component/Admin/AdminSpsMessage';
@@ -79,6 +77,46 @@ import AdminCitizensAlert from './component/Admin/AdminCitizensAlert';
 
 
 
+import AdminSpsMessageForm from './component/Admin/AdminSpsMessageForm';
+import AdminSpsForm from './component/Admin/AdminSpsForm';
+import AdminHeadlineForm from './component/Admin/AdminHeadlineForm';
+import AdminEmergencyContactsForm from './component/Admin/AdminEmergencyContactsForm';
+import AdminAddPhotosToAlbumForm from './component/Admin/AdminAddPhotosToAlbumForm';
+import AdminAddAlbumNameForm from './component/Admin/AdminAddAlbumNameForm';
+import AdminCitizensAlertForm from './component/Admin/AdminCitizensAlertForm';
+import AdminCircularsForm from './component/Admin/AdminCircularsForm';
+import AdminImprotantWebsitesForm from './component/Admin/AdminImprotantWebsitesForm';
+import AdminGazetteForm from './component/Admin/AdminGazetteForm';
+import AdminGradationListForm from './component/Admin/AdminGradationListForm';
+import AdminAnnualAdminReportForm from './component/Admin/AdminAnnualAdminReportForm';
+import AdminInfoProvoidedUnderRTIForm from './component/Admin/AdminInfoProvoidedUnderRTIForm';
+import AdminRightToInformationForm from './component/Admin/AdminRightToInformationForm';
+import AdminTransferOrderForm from './component/Admin/AdminTransferOrderForm';
+import AdminPromotionOrderForm from './component/Admin/AdminPromotionOrderForm';
+import AdminBMIForm from './component/Admin/AdminBMIForm';
+import AdminRecuirtmentForm from './component/Admin/AdminRecuirtmentForm';
+import AdminCompassionListForm from './component/Admin/AdminCompassionListForm';
+import AdminRtirementListForm from './component/Admin/AdminRtirementListForm';
+import AdminIncrementListForm from './component/Admin/AdminIncrementListForm';
+import AdminMedicalBillsForm from './component/Admin/AdminMedicalBillsForm';
+import AdminDailyDutyChartFlashForm from './component/Admin/AdminDailyDutyChartFlashForm';
+import AdminCrimeInformationForm from './component/Admin/AdminCrimeInformationForm';
+import AdminTenderForm from './component/Admin/AdminTenderForm';
+import AdminPressReleaseForm from './component/Admin/AdminPressReleaseForm';
+import AdminSubDivisionForm from './component/Admin/AdminSubDivisionForm';
+import AdminOfficerTypeForm from './component/Admin/AdminOfficerTypeForm';
+import AdminOfficerForm from './component/Admin/AdminOfficerForm';
+import AdminSpecialUnitForm from './component/Admin/AdminSpecialUnitForm';
+import AdminContactOfficerDivisionForm from './component/Admin/AdminContactOfficerDivisionForm';
+import AdminContactOfficerForm from './component/Admin/AdminContactOfficerForm';
+import AdminPoliceStationForm from './component/Admin/AdminPoliceStationForm';
+import AdminWomenCityTypesForm from './component/Admin/AdminWomenCityTypesForm';
+import AdminWomenHelpDeskForm from './component/Admin/AdminWomenHelpDeskForm';
+import AdminPositiveStoryForm from './component/Admin/AdminPositiveStoryForm';
+import AdminUserForm from './component/Admin/AdminUserForm';
+
+
+
 
 
 
@@ -86,13 +124,13 @@ import AdminCitizensAlert from './component/Admin/AdminCitizensAlert';
 
 
 function App() {
- 
+
   return (
     <>
-          <BrowserRouter>
- 
-          <Routes>
-       
+      <BrowserRouter>
+
+        <Routes>
+
           <Route path='/' index element={<Home />} />
           <Route path='/contactus' index element={<Contactus />} />
           <Route path='/policerecruitment' index element={<PoliceRecruitment />} />
@@ -105,78 +143,117 @@ function App() {
           <Route path='/absconders' index element={<Absconders />} />
           <Route path='/policewelfare' index element={<PoliceWelfare />} />
           <Route path='/citizencorner' index element={<CitizenCorner />} />
-          <Route path='/medicafacilities' index element={<MedicaFacilities/>} />
-          <Route path='/gazette' index element={<Gazette/>} />
-          <Route path='/promotionorders' index element={<PromotionOrders/>} />
-          <Route path='/bmi' index element={<Bmi/>} />
-          <Route path='/circulars' index element={<Circulars/>} />
-          <Route path='/gradationlist' index element={<GradationList/>} />
-          <Route path='/beyondcallofduty' index element={<BeyondCallOfDuty/>} />
-          <Route path='/buddybench' index element={<BuddyBench/>} />
-          <Route path='/flash' index element={<Flash/>} />
-          <Route path='/crimeinformation' index element={<CrimeInformation/>} />
-          <Route path='/firregistration' index element={<FIRRegistration/>} />
-          <Route path='/missingperson' index element={<MissingPerson/>} />
-          <Route path='/tender' index element={<Tender/>} />
-          <Route path='/helpline' index element={<HelpLine/>} />
-          <Route path='/photogallery' index element={<PhotoGallery/>} />
-          <Route path='/atrocitycases' index element={<AtrocityCases/>} />
-          <Route path='/onlinecomplaint' index element={<OnlineComplaint/>} />
-          <Route path='/righttoinformation' index element={<RightToInformation/>} />
-          <Route path='/pressrelease' index element={<PressRelease/>} />          
-          <Route path='/womenhelpdesk' index element={<WomenHelpDesk/>} />
-          <Route path='/touristhelpdesks' index element={<TouristHelpDesks/>} />
+          <Route path='/medicafacilities' index element={<MedicaFacilities />} />
+          <Route path='/gazette' index element={<Gazette />} />
+          <Route path='/promotionorders' index element={<PromotionOrders />} />
+          <Route path='/bmi' index element={<Bmi />} />
+          <Route path='/circulars' index element={<Circulars />} />
+          <Route path='/gradationlist' index element={<GradationList />} />
+          <Route path='/beyondcallofduty' index element={<BeyondCallOfDuty />} />
+          <Route path='/buddybench' index element={<BuddyBench />} />
+          <Route path='/flash' index element={<Flash />} />
+          <Route path='/crimeinformation' index element={<CrimeInformation />} />
+          <Route path='/firregistration' index element={<FIRRegistration />} />
+          <Route path='/missingperson' index element={<MissingPerson />} />
+          <Route path='/tender' index element={<Tender />} />
+          <Route path='/helpline' index element={<HelpLine />} />
+          <Route path='/photogallery' index element={<PhotoGallery />} />
+          <Route path='/atrocitycases' index element={<AtrocityCases />} />
+          <Route path='/onlinecomplaint' index element={<OnlineComplaint />} />
+          <Route path='/righttoinformation' index element={<RightToInformation />} />
+          <Route path='/pressrelease' index element={<PressRelease />} />
+          <Route path='/womenhelpdesk' index element={<WomenHelpDesk />} />
+          <Route path='/touristhelpdesks' index element={<TouristHelpDesks />} />
 
-          
 
-           {/* admin start */}
-       <Route path='/admindashboard' element={<AdminDashboard />} />
-       <Route path='/adminuser' element={<AdminUser />} />
-       <Route path='/adminticketview' element={<AdminTicketsView />} />
-       <Route path='/adminheadline' element={<AdminHeadline />} />
-       <Route path='/adminspsmessage' element={<AdminSpsMessage />} />
-       <Route path='/adminsps' element={<AdminSps/>} />
-       <Route path='/adminpositivestory' element={<AdminPositiveStory/>} />
-       <Route path='/adminwomenhelp' element={<AdminWomenHelp/>} />
-       <Route path='/adminwomencitytypes' element={<AdminWomenCityTypes/>} />
-       <Route path='/admincontactofficer' element={<AdminContactOfficer/>} />
-       <Route path='/admincontactofficedivision' element={<AdminContactOfficeDivision/>} />
-       <Route path='/adminpolicestation' element={<AdminPoliceStation/>} />
-       <Route path='/adminspecialunit' element={<AdminSpecialUnit/>} />
-       <Route path='/adminofficer' element={<AdminOfficer/>} />
-       <Route path='/adminofficertype' element={<AdminOfficerType/>} />
-       <Route path='/adminsubdivision' element={<AdminSubDivision/>} />
-       <Route path='/adminpressrelease' element={<AdminPressRelease/>} />
-       <Route path='/admintender' element={<AdminTender/>} />
-       <Route path='/admincrimeinformation' element={<AdminCrimeInformation/>} />
-       <Route path='/admindailydutychartflash' element={<AdminDailyDutyChartFlash/>} />
-       <Route path='/adminmedicalbills' element={<AdminMedicalBills/>} />
-       <Route path='/adminrtirementlist' element={<AdminRtirementList/>} />
-       <Route path='/adminincrementlist' element={<AdminIncrementList/>} />
-       <Route path='/admincompassionlist' element={<AdminCompassionList/>} />
-       <Route path='/adminrecuirtment' element={<AdminRecuirtment/>} />
-       <Route path='/adminbmi' element={<AdminBMI/>} />
-       <Route path='/adminpromotionorder' element={<AdminPromotionOrder/>} />
-       <Route path='/admincrimestatics' element={<AdminCrimeStatics/>} />
-       <Route path='/admintransferorder' element={<AdminTransferOrder/>} />
-       <Route path='/admingradationlist' element={<AdminGradationList/>} />
-       <Route path='/admingazette' element={<AdminGazette/>} />
-       <Route path='/adminrighttoinformation' element={<AdminRightToInformation/>} />
-       <Route path='/admininfoprovoidedUnderrti' element={<AdminInfoProvoidedUnderRTI/>} />
-       <Route path='/adminannualadminreport' element={<AdminAnnualAdminReport/>} />
-       <Route path='/adminimprotantwebsites' element={<AdminImprotantWebsites/>} />
-       <Route path='/admincirculars' element={<AdminCirculars/>} />
-       <Route path='/admincitizensalert' element={<AdminCitizensAlert/>} />
-       <Route path='/adminemergencycontacts' element={<AdminEmergencyContacts/>} />
-       <Route path='/adminaddalbumname' element={<AdminAddAlbumName/>} />
-       <Route path='/adminaddphotostoalbum' element={<AdminAddPhotosToAlbum/>} />
-      {/* admin end*/}
+
+          {/* admin start */}
+          <Route path='/admindashboard' element={<AdminDashboard />} />
+          <Route path='/adminuser' element={<AdminUser />} />
+          <Route path='/adminheadline' element={<AdminHeadline />} />
+          <Route path='/adminspsmessage' element={<AdminSpsMessage />} />
+          <Route path='/adminsps' element={<AdminSps />} />
+          <Route path='/adminpositivestory' element={<AdminPositiveStory />} />
+          <Route path='/adminwomenhelp' element={<AdminWomenHelp />} />
+          <Route path='/adminwomencitytypes' element={<AdminWomenCityTypes />} />
+          <Route path='/admincontactofficer' element={<AdminContactOfficer />} />
+          <Route path='/admincontactofficedivision' element={<AdminContactOfficeDivision />} />
+          <Route path='/adminpolicestation' element={<AdminPoliceStation />} />
+          <Route path='/adminspecialunit' element={<AdminSpecialUnit />} />
+          <Route path='/adminofficer' element={<AdminOfficer />} />
+          <Route path='/adminofficertype' element={<AdminOfficerType />} />
+          <Route path='/adminsubdivision' element={<AdminSubDivision />} />
+          <Route path='/adminpressrelease' element={<AdminPressRelease />} />
+          <Route path='/admintender' element={<AdminTender />} />
+          <Route path='/admincrimeinformation' element={<AdminCrimeInformation />} />
+          <Route path='/admindailydutychartflash' element={<AdminDailyDutyChartFlash />} />
+          <Route path='/adminmedicalbills' element={<AdminMedicalBills />} />
+          <Route path='/adminrtirementlist' element={<AdminRtirementList />} />
+          <Route path='/adminincrementlist' element={<AdminIncrementList />} />
+          <Route path='/admincompassionlist' element={<AdminCompassionList />} />
+          <Route path='/adminrecuirtment' element={<AdminRecuirtment />} />
+          <Route path='/adminbmi' element={<AdminBMI />} />
+          <Route path='/adminpromotionorder' element={<AdminPromotionOrder />} />
+          <Route path='/admincrimestatics' element={<AdminCrimeStatics />} />
+          <Route path='/admintransferorder' element={<AdminTransferOrder />} />
+          <Route path='/admingradationlist' element={<AdminGradationList />} />
+          <Route path='/admingazette' element={<AdminGazette />} />
+          <Route path='/adminrighttoinformation' element={<AdminRightToInformation />} />
+          <Route path='/admininfoprovoidedunderrti' element={<AdminInfoProvoidedUnderRTI />} />
+          <Route path='/adminannualadminreport' element={<AdminAnnualAdminReport />} />
+          <Route path='/adminimprotantwebsites' element={<AdminImprotantWebsites />} />
+          <Route path='/admincirculars' element={<AdminCirculars />} />
+          <Route path='/admincitizensalert' element={<AdminCitizensAlert />} />
+          <Route path='/adminemergencycontacts' element={<AdminEmergencyContacts />} />
+          <Route path='/adminaddalbumname' element={<AdminAddAlbumName />} />
+          <Route path='/adminaddphotostoalbum' element={<AdminAddPhotosToAlbum />} />
+
+          <Route path='/adminemergencycontactsform' element={<AdminEmergencyContactsForm />} />
+          <Route path='/adminspsform' element={<AdminSpsForm />} />
+          <Route path='/adminspsmessageform' element={<AdminSpsMessageForm />} />
+          <Route path='/adminheadlineform' element={<AdminHeadlineForm />} />
+          <Route path='/adminaddphotostoalbumform' element={<AdminAddPhotosToAlbumForm />} />
+          <Route path='/adminaddalbumnameform' element={<AdminAddAlbumNameForm />} />
+          <Route path='/Admincircularsform' element={<AdminCircularsForm />} />
+          <Route path='/admincitizensalertform' element={<AdminCitizensAlertForm />} />
+          <Route path='/adminimprotantwebsitesform' element={<AdminImprotantWebsitesForm />} />
+          <Route path='/admingazetteform' element={<AdminGazetteForm />} />
+          <Route path='/admingradationlistform' element={<AdminGradationListForm />} />
+          <Route path='/adminannualadminreportform' element={<AdminAnnualAdminReportForm />} />
+          <Route path='/admininfopprovoidedunderrtiform' element={<AdminInfoProvoidedUnderRTIForm />} />
+          <Route path='/adminrighttoinformationform' element={<AdminRightToInformationForm />} />
+          <Route path='/admintransferorderform' element={<AdminTransferOrderForm />} />
+          <Route path='/adminpromotionorderform' element={<AdminPromotionOrderForm />} />
+          <Route path='/adminbmiform' element={<AdminBMIForm />} />
+          <Route path='/adminrecuirtmentform' element={<AdminRecuirtmentForm />} />
+          <Route path='/admincompassionlistform' element={<AdminCompassionListForm />} />
+          <Route path='/adminrtirementlistform' element={<AdminRtirementListForm />} />
+          <Route path='/adminincrementlistform' element={<AdminIncrementListForm />} />
+          <Route path='/adminmedicalbillsform' element={<AdminMedicalBillsForm />} />
+          <Route path='/admindailydutychartflashform' element={<AdminDailyDutyChartFlashForm />} />
+          <Route path='/admincrimeinformationform' element={<AdminCrimeInformationForm />} />
+          <Route path='/admintenderform' element={<AdminTenderForm />} />
+          <Route path='/adminpressreleaseform' element={<AdminPressReleaseForm />} />
+          <Route path='/adminsubdivisionform' element={<AdminSubDivisionForm />} />
+          <Route path='/adminofficertypeform' element={<AdminOfficerTypeForm />} />
+          <Route path='/adminofficerform' element={<AdminOfficerForm />} />
+          <Route path='/adminspecialunitform' element={<AdminSpecialUnitForm />} />
+          <Route path='/admincontactofficerdivisionform' element={<AdminContactOfficerDivisionForm />} />
+          <Route path='/adminpolicestationform' element={<AdminPoliceStationForm />} />
+          <Route path='/adminwomencitytypesform' element={<AdminWomenCityTypesForm />} />
+          <Route path='/adminwomenhelpdeskform' element={<AdminWomenHelpDeskForm />} />
+          <Route path='/adminpositivestoryform' element={<AdminPositiveStoryForm />} />
+          <Route path='/adminuserform' element={<AdminUserForm />} />
+
+
+
+          {/* admin end*/}
         </Routes>
 
       </BrowserRouter>
-     
 
-    
+
+
     </>
 
   );
